779c164845880bf5b84b0a15570062314db6b1c5
codewin <codewin19@gmail.com>
2021-01-22 21:19:52 +0530

    Php class one

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <style>
+        body{
+            display: flex;
+            justify-content: center;
+            align-items:center;
+            height:100vh;
+        }
+
+        div{
+            display:flex;
+            flex-direction: column;
+           
+        }
+
+        div input{
+            margin:10px;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- <h1>This is home page</h1> -->
+    <form action="process.php" method="get">
+    <div>
+        <input type="number" name="fname" placeholder="First Number">
+        <input type="number" name="sname"  placeholder="second Number">
+        <input type="submit" value="Add">
+    </div>
+    </form>
+</body>
+
+</html>
---
+++ process.php
@@ -0,0 +1,12 @@
+<?php
+
+    $fname = $_GET['fname'];
+    $sname = $_GET['sname'];
+
+    $result = $fname + $sname;
+
+
+    echo "Result is ".$result;
+
+
+?>
